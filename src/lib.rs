a99287ba722564fcb1a917f51809c9d8f278537a
Xuan Hieu <nguyenxuanhieuktb10@gmail.com>
2025-05-08 18:11:51 +0700

    feat: add new zk (#5)

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@
 use thiserror::Error;
 
 mod common;
+pub mod dlog_with_el_gamal_commitment;
 pub mod group_element_vs_paillier_encryption_in_range;
 pub mod multiexp;
 pub mod no_small_factor;
@@ -15,8 +16,6 @@ pub mod paillier_affine_operation_in_range;
 pub mod paillier_blum_modulus;
 pub mod paillier_encryption_in_range;
 pub mod paillier_encryption_in_range_with_el_gamal;
-
-
 #[cfg(test)]
 mod curve;
 
